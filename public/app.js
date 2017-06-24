7fbcfcd6de8ecfed79270c76c247f603398bc723
Marilena Alexe <marilena.alexe@gmail.com>
2017-06-24 15:11:53 +0100

    Add <il> favourite food and append it to <ul> cat using JS

--- public/app.js
+++ public/app.js
@@ -10,20 +10,28 @@ var createLiName = function(name) {
   return li;
 }
 
-var appendElements = function(list, ul, li) {
+var createLiFavFood = function(food) {
+  var li = document.createElement('li');
+  li.innerText = "Favourite food: " + food;
+  return li;
+}
+
+var appendElements = function(list, ul, li, li1) {
   ul.appendChild(li);
+  ul.appendChild(li1);
   list.appendChild(ul);
 }
 
-var addNewCat = function(name) {
+var addNewCat = function(name, food) {
   var ul = createUlElement();
   var li = createLiName(name);
+  var li1 = createLiFavFood(food);
   var section = document.getElementById("cats");
-  appendElements(section, ul, li);
+  appendElements(section, ul, li, li1);
 }
 
 var app = function() {
-  addNewCat("Roxy");
+  addNewCat("Roxy", "Milk");
 }
 
 window.onload = app;
